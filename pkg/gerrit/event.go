3626fe78c7b16348aa9f95668d3cd301229b6177
Jalander Ramagiri <jalander.ramagiri@est.tech>
2024-03-19 16:07:37 +0000

    Initial changes to translate branch and repository events
    Signed-off-by: Jalander Ramagiri <jalander.ramagiri@est.tech>

---
+++ pkg/gerrit/event.go
@@ -0,0 +1,86 @@
+/*
+Copyright (C) 2024 Nordix Foundation.
+For a full list of individual contributors, please see the commit history.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package gerrit
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type GerritEvent struct {
+	Event   string
+	RepoURL string
+}
+
+func NewGerritEvent(event string, repoURL string) (pEvent *GerritEvent) {
+	pEvent = &GerritEvent{event, repoURL}
+	return
+}
+
+func HandleTranslateGerritEvent(event string, header http.Header) (string, error) {
+	log.Printf("Handle translation into CDEvent from Gerrit event %s\n", event)
+	repoURL := ""
+	if header.Get("X-Origin-Url") != "" {
+		repoURL = header.Get("X-Origin-Url")
+	}
+	gerritEvent := NewGerritEvent(event, repoURL)
+	cdEvent, err := gerritEvent.TranslateIntoCDEvent()
+	if err != nil {
+		log.Printf("Error translating Gerrit event into CDEvent %s\n", err)
+		return "", err
+	}
+	log.Printf("Gerrit Event translated into CDEvent %s\n", cdEvent)
+	return cdEvent, nil
+}
+
+func (pEvent *GerritEvent) TranslateIntoCDEvent() (string, error) {
+	eventMap := make(map[string]interface{})
+	cdEvent := ""
+	err := json.Unmarshal([]byte(pEvent.Event), &eventMap)
+	if err != nil {
+		log.Println("Error occurred while Unmarshal gerritEvent data into gerritEvent map", err)
+		return "", err
+	}
+	eventType := eventMap["type"]
+	log.Printf("handling translating to CDEvent from Gerrit Event type: %s\n", eventType)
+
+	switch eventType {
+	case "project-created":
+		cdEvent, err = pEvent.HandleProjectCreatedEvent()
+		if err != nil {
+			return "", err
+		}
+	case "ref-updated":
+		cdEvent, err = pEvent.HandleRefUpdatedEvent()
+		if err != nil {
+			return "", err
+		}
+	case "project-head-updated":
+		cdEvent, err = pEvent.HandleProjectHeadUpdatedEvent()
+		if err != nil {
+			return "", err
+		}
+	default:
+		log.Printf("Not handling CDEvent translation for Gerrit event type: %s\n", eventMap["type"])
+		return "", errors.New(fmt.Sprintf("Gerrit event %s, not supported for translation", eventType))
+	}
+	return cdEvent, nil
+}
---
+++ pkg/translator.go
@@ -0,0 +1,50 @@
+/*
+Copyright (C) 2024 Nordix Foundation.
+For a full list of individual contributors, please see the commit history.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package main
+
+import (
+	"github.com/cdevents/gerrit-translator/pkg/gerrit"
+	"github.com/cdevents/webhook-adapter/pkg/cdevents"
+	"github.com/hashicorp/go-plugin"
+	"log"
+	"net/http"
+)
+
+type EventTranslator struct{}
+
+// TranslateEvent Invoked from external application to translate Gerrit event into CDEvent
+func (EventTranslator) TranslateEvent(event string, headers http.Header) (string, error) {
+	log.Println("Serving from gerrit-translator plugin")
+	cdEvent, err := gerrit.HandleTranslateGerritEvent(event, headers)
+	if err != nil {
+		return "", err
+	}
+	return cdEvent, nil
+}
+
+func main() {
+	plugin.Serve(&plugin.ServeConfig{
+		HandshakeConfig: cdevents.Handshake,
+		Plugins: map[string]plugin.Plugin{
+			"gerrit-translator-cdevents": &cdevents.TranslatorGRPCPlugin{Impl: &EventTranslator{}},
+		},
+
+		// A non-nil value here enables gRPC serving for this plugin...
+		GRPCServer: plugin.DefaultGRPCServer,
+	})
+}
